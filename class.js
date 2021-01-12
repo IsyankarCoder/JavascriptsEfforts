ab69049d0add161b326579a0eb11aac4e94becaf
isyankarcoder <gencvolkan@gmail.com>
2021-01-12 22:54:41 +0300

    ...

--- class.js
+++ class.js
@@ -1,8 +1,5 @@
 var u =(function () {
-    function User(name) {
-        function User() {
-        
-        }
+    function User(name) { 
         this.name += name;
     }
     User.prototype.sayHi = function(){
@@ -40,3 +37,57 @@ function MakeClass(obj) {
 let dynaUser = MakeClass("Volkan");
 new dynaUser().sayHi();
 
+
+
+class getClass{
+    constructor(name) {
+        this.name = name;
+    }
+
+    //computed column
+    ['say + Hi']() {
+        console.log("Hello");
+    }
+
+    sayHello = function (ong) {
+        console.log(`Hello, ${ong} , ${this.name}`);
+    }
+
+    get name(){
+        return this._name;
+    }    
+    set name(value) {
+        if (value.length < 4) {
+            console.log("Kısan");
+            return;
+        }
+        this._name = value;
+    }
+}
+
+let ml = new getClass("Volkan GENÇ");
+
+console.log(ml.name);
+
+ml.name = "";
+ml["say + Hi"]();
+ml.sayHello("tjj");
+
+
+class Button
+{
+     
+    constructor(value) {
+        this.value = value;
+        this.name2 = value;
+    }
+
+    click() {
+        console.log(`Hello,  ${this.value},${this.name2}`);
+    }
+    
+}
+
+let btn = new Button(55); 
+btn.name2 = "gg";
+setTimeout(() => btn.click, 100);
