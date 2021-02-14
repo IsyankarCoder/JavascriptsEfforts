fb306b5da0572d47f1528d328ba1eb06df593810
isyankarcoder <gencvolkan@gmail.com>
2021-02-14 14:44:12 +0300

    export class

--- modules/main.js
+++ modules/main.js
@@ -1,3 +1,5 @@
-import { sayHi } from './sayHi.js';
+import { sayHi,MyVolkan } from './sayHi.js';
 console.log(import.meta.url);
 sayHi("Volkan");
+let a = new MyVolkan();
+a.sayHello();
--- modules/sayHi.js
+++ modules/sayHi.js
@@ -2,3 +2,12 @@ export function sayHi(user) {
     console.log(import.meta.url);
     console.log(`Hello, ${user}!`);
 }
+
+export class MyVolkan{
+    constructor() {
+        
+    }
+    sayHello() {
+        console.log("myVolkan Class");
+    }
+}
